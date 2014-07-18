804dd134cfb66b4d6d423d52fa5b1cd80f331aec
Ryan Morshead <ryan.morshead@gmail.com>
2014-07-18 15:12:04 -0700

    dont need configure file

--- .configureGit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-git config --global user.name "Ryan Morshead"
-git config --global user.email "ryan.morshead@gmail.com"
-git config --global color.ui "auto"
-git config --global core.editor "nano"
